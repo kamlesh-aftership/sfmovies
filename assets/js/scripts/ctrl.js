85412d118218eadec5e2ff218036b533d52c5ecc
kamlesh-aftership <kamlesh@algorist.in>
2017-01-06 15:54:27 +0530

    Update ctrl.js

--- assets/js/scripts/ctrl.js
+++ assets/js/scripts/ctrl.js
@@ -1,6 +1,21 @@
 'use strict';
 
 const app = angular.module('sfmovieApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+let map;
+let markers = [];
+
+app.config(function ($provide, $httpProvider) {
+	$provide.factory('ErrorInterceptor', function ($q) {
+		return {
+			responseError: function (rejection) {
+				alert('an error occured. Please try again after some time.');
+				return $q.reject(rejection);
+			}
+		};
+	});
+	$httpProvider.interceptors.push('ErrorInterceptor');
+});
+
 app.controller('locationController', function ($scope, $log, $http) {
 	const self = this;
 	self.querySearch = querySearch;
@@ -11,6 +26,7 @@ app.controller('locationController', function ($scope, $log, $http) {
 	self.loadScript = loadScript;
 	self.ChangeGoogleMapsLanguage = ChangeGoogleMapsLanguage;
 
+	// api call for get all movies
 	$http.get('/location').then(function (response) {
 		let allStates = response.data;
 		self.states = allStates.map(function (state) {
@@ -21,17 +37,24 @@ app.controller('locationController', function ($scope, $log, $http) {
 		});
 	});
 
+	// to handle situation when use enter invalid name which doesn't exist
 	function newState(state) {
 		alert('Sorry! You will need to create a Constitution for ' + state + ' first!');
 	}
+
+	// this function is used for serching keyword
 	function querySearch(query) {
 		let results = query ? self.states.filter(createFilterFor(query)) : self.states;
 		return results;
 	}
 
+	// tracks the user input search text
 	function searchTextChange(text) {
 		$log.info('Text changed to ' + text);
-	} function selectedItemChange(item) {
+	}
+
+	// get movie when use enter movie name
+	function selectedItemChange(item) {
 		if (item !== undefined) {
 			for (let i = 0; i < markers.length; i++) {
 				markers[i].setMap(null);
@@ -44,6 +67,8 @@ app.controller('locationController', function ($scope, $log, $http) {
 			});
 		}
 	}
+
+	// filter the movie name based on user input
 	function createFilterFor(query) {
 		let lowercaseQuery = angular.lowercase(query);
 		return function filterFn(state) {
@@ -54,24 +79,10 @@ app.controller('locationController', function ($scope, $log, $http) {
 	$scope.availableLocales = {
 		'en': 'English',
 		'zh': 'Chinese'};
-	$scope.selectedLocale = sessionStorage.getItem("lang");
+	$scope.selectedLocale = sessionStorage.getItem('lang');
 });
 
-app.config(function ($provide, $httpProvider) {
-	$provide.factory('ErrorInterceptor', function ($q) {
-		return {
-			responseError: function (rejection) {
-				alert('an error occured. Please try again after some time.');
-				console.log(rejection);
-				return $q.reject(rejection);
-			}
-		};
-	});
-	$httpProvider.interceptors.push('ErrorInterceptor');
-});
-
-let map;
-let markers = [];
+// initializes the google map
 function initialize() {
 	let mapOptions = {
 		zoom: 10,
@@ -85,6 +96,7 @@ function initialize() {
 	});
 }
 
+// loads the script to change google-map language based on user selection
 function loadScript() {
 	let newlang = sessionStorage.getItem('lang');
 	let script = document.createElement('script');
@@ -100,11 +112,13 @@ function loadScript() {
 
 window.onload = loadScript;
 
+// set the language name in session and reloads the page
 function ChangeGoogleMapsLanguage(language) {
 	sessionStorage.setItem('lang', language);
 	window.location.reload(true);
 }
 
+// create the marker to show locations on map
 function createmarker(obj) {
 	let marker = new google.maps.Marker({
 		position: {lat: parseFloat(obj.coords.latitude), lng: parseFloat(obj.coords.longitude)},
